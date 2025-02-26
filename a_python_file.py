d2d00a5ef132f753927d5b2b031c2e35cac67eb1
Sebastian <sebastian@macheight.com>
2025-02-26 11:12:04 -0500

    second commit for feature b this commit has a description below
    description of this commit

--- a_python_file.py
+++ a_python_file.py
@@ -2,3 +2,4 @@ a_variable = "I am a variable"
 another_variable = "I am another variable"
 
 variable_from_feature_b = "I am a variable from feature b"
+second_variable_from_feature_b = "I also a variable from feature b"
